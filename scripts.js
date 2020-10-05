b7c8e22af1b08a965d7e06e045188bf9e2c1dd3a
Paula Suarez <Paula.Suarez@mainevent.com>
2020-10-05 09:51:28 -0500

    Adding updates

---
+++ scripts.js
@@ -0,0 +1,78 @@
+// Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+
+// For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
+// Bonus: Can you do this in one pass?
+
+//check for pairs with nested loop
+
+
+
+
+// function findSum() {
+
+//     for (let i = 0; i < numbers.length; i++) {
+//         console.log(i);
+//         for (let j = 1; j < numbers.length; j++) {
+//             if (i != j && numbers[i] + numbers[j] == k) {
+//                 console.log('true');
+//                 return true
+//             }
+//             console.log('false');
+//             return false
+
+//         }
+//     }
+
+// }
+
+// for (let i of numbers) {
+//     console.log(i);
+//     for (let j of numbers) {
+//         console.log(j);
+//         if (i != j && numbers[i] + numbers[j] == k) {
+//             console.log('true')
+//         } else {
+//             console.log('false');
+
+
+//         }
+//     }
+// }
+
+
+let numbers = [10, 15, 3, 7];
+let k = 17;
+
+function findSum() {
+    for (let i = 0; i < numbers.length; i++) {
+    // for (let i of numbers) {
+        console.log(numbers[i]);
+        for (let j = numbers.length - 1; j >= 0; j--) {
+            console.log(numbers[j]);
+            if (numbers[i] + numbers[j] == k) {
+                console.log('true');
+                return true;
+            } else {
+                console.log('false');
+                return false;
+            }
+        }
+    }
+}
+findSum();
+
+
+
+
+
+
+
+
+
+// def two_sum(lst, k):
+//     for i in range(len(lst)):
+//         for j in range(len(lst)):
+//             if i != j and lst[i] + lst[j] == k:
+//                 return True
+//   return 
